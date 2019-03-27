d2834d72e93fa9c370b41a54b26464043fa7f69f
Angelo Leto <angelo@Angelos-MBP.lan>
2019-03-27 15:25:18 +0100

    elasticsearch 6.7 ; updated doc ; testes scripts

--- scripts/api_test/loadDecisionTableFile.sh
+++ scripts/api_test/loadDecisionTableFile.sh
@@ -2,7 +2,7 @@
 
 PORT=${1:-8443}
 INDEX_NAME=${2:-'index_getjenny_english_0'}
-FILENAME=${3:-"$(readlink -e ../../doc/decision_table_starchat_doc.csv)"}
+FILENAME=${3:-"../../doc/decision_table_starchat_doc.csv"}
 curl -k -v -H "Authorization: Basic $(echo -n 'test_user:p4ssw0rd' | base64)" \
  -X POST --form "csv=@${FILENAME}" "https://localhost:${PORT}/${INDEX_NAME}/decisiontable/upload_csv"
 
